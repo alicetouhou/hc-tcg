bf065028b3a9c0b41a4b467fc326d376b6bbe76d
Benji42 <cheezyanimations@gmail.com>
2025-02-19 21:58:24 -0500

    update

--- client/src/app/login/login.tsx
+++ client/src/app/login/login.tsx
@@ -10,7 +10,7 @@ import {
 	getConnectingMessage,
 	getErrorType,
 } from 'logic/session/session-selectors'
-import React from 'react'
+import {useEffect, useRef, useState} from 'react'
 import {useSelector} from 'react-redux'
 import css from './login.module.scss'
 
@@ -30,39 +30,107 @@ const Login = () => {
 	const connecting = useSelector(getConnecting)
 	const errorType = useSelector(getErrorType)
 	const connectingMessage = useSelector(getConnectingMessage)
+	const playerNameRef = useRef<HTMLInputElement>(null)
+	const [page, setPage] = useState<'login' | 'sync'>('login')
 
-	const handlePlayerName = (ev: React.SyntheticEvent<HTMLFormElement>) => {
-		ev.preventDefault()
-		const name = ev.currentTarget.playerName.value.trim()
+	const handlePlayerName = () => {
+		if (!playerNameRef.current) return
+		const name = playerNameRef.current.value
+		console.log(name)
 		if (name.length > 0) dispatch({type: localMessages.LOGIN, name: name})
+	}
+
+	useEffect(() => {
+		window.addEventListener('keydown', handleKeyPress)
+		return () => {
+			window.removeEventListener('keydown', handleKeyPress)
+		}
+	}, [handleKeyPress])
+
+	function handleKeyPress(e: any) {
+		if (page === 'sync' || connecting) return
+		if (e.key !== 'Enter') return
+		handlePlayerName()
+	}
+
+	if (connecting) {
+		return (
+			<div className={css.loginBackground}>
+				<div className={css.loginContainer}>
+					<TcgLogo />
+					<div className={css.connecting}>
+						<Spinner />
+						<p>{connectingMessage}</p>
+					</div>
+				</div>
+			</div>
+		)
 	}
 
 	return (
 		<div className={css.loginBackground}>
 			<div className={css.loginContainer}>
 				<TcgLogo />
-				{connecting ? (
-					<div className={css.connecting}>
-						<Spinner />
-						<p>{connectingMessage}</p>
-					</div>
-				) : (
-					<form className={css.nameForm} onSubmit={handlePlayerName}>
-						<div className={css.customInput}>
-							<input
-								maxLength={25}
-								name="playerName"
-								placeholder=" "
-								autoFocus
-								id="username"
-							></input>
-							<label htmlFor="username">Player Name</label>
+				<div className={css.text}>
+					{page === 'login' && (
+						<div>
+							<p>
+								HC TCG Online is an online version of the HC TCG game created by
+								Vintage Beef for the Hermitcraft Server. If this is your first
+								time playing, please enter your name and click “Play”!
+							</p>
+							<div className={css.nameForm}>
+								<input
+									maxLength={25}
+									name="playerName"
+									placeholder="Player Name"
+									autoFocus
+									id="username"
+									ref={playerNameRef}
+								></input>
+								<Button
+									variant="default"
+									type="submit"
+									onClick={() => handlePlayerName()}
+								>
+									Play
+								</Button>
+							</div>
 						</div>
-						<Button variant="stone" type="submit">
-							Next
-						</Button>
-					</form>
-				)}
+					)}
+					<div className={css.clickable} onClick={() => setPage('sync')}>
+						If you've already logged in on another device, click on this box to
+						sync your profile.
+					</div>
+					{page === 'sync' && (
+						<div>
+							<p>These are the instructions on how to sync your account.</p>
+							<div>
+								<input
+									maxLength={25}
+									name="playerName"
+									placeholder="User ID"
+									autoFocus
+									id="username"
+								></input>
+								<input
+									maxLength={25}
+									name="playerName"
+									placeholder="Secret"
+									autoFocus
+									id="username"
+								></input>
+								<Button
+									variant="default"
+									type="submit"
+									onClick={() => handlePlayerName()}
+								>
+									Sync
+								</Button>
+							</div>
+						</div>
+					)}
+				</div>
 				{errorType && <ErrorBanner>{getLoginError(errorType)}</ErrorBanner>}
 				<VersionLinks />
 				<Beef />
